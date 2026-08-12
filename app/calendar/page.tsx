3c6cc8b7197773e176a5e8fa8ae659ae39400a75
Costa Clean <costacleanbcn@gmail.com>
2026-08-12 12:47:15 +0200

    fix: localize calendar copy

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+﻿"use client";
 
 import Link from "next/link";
 import { useEffect, useMemo, useState } from "react";
@@ -394,3 +394,5 @@ export default function CalendarPage() {
   const todayKey = useCurrentLocalDateKey();
   return todayKey ? <CalendarContent todayKey={todayKey} /> : null;
 }
+
+
